b325f3cbbdfbe6e814860c3bedb08de07637ca06
crakacr-alt <crakacr@gmail.com>
2026-09-26 12:04:33 +0300

    labscript: define language version 0.1

--- pnp_lab/labscript/__init__.py
+++ pnp_lab/labscript/__init__.py
@@ -3,7 +3,10 @@
 from .language import Language, detect_language, translate_source
 from .runtime import LabRuntime, LabScriptError, run_source
 
+LABSCRIPT_VERSION = "0.1"
+
 __all__ = [
+    "LABSCRIPT_VERSION",
     "LabRuntime",
     "LabScriptError",
     "Language",
